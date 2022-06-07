e302ff6bda297453730cb28d16ad3d0407bba168
RIFQIBOTZ <putikhatiriskia@gmail.com>
2022-06-07 11:45:28 +0700

    2

---
+++ settings.js
@@ -0,0 +1,66 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.owner = ['6285377536527','6285377536527']
+global.premium = ['6285377536527']
+global.packname = ''
+global.author = ''
+global.sessionName = 'kirbotz'
+global.namabotnya = 'Mbemcans Botz'
+global.namaownernya = 'Hanna Nur Febriana'
+global.dapkey = 'Kirbotz123'
+global.lolkey = 'ripki'
+global.kirkey = 'KirBotz'
+global.wame = 'https://wa.me6285377536527'
+global.gckirbotz = 'https://chat.whatsapp.com/BxHapomSAFH6YGUAvuq3aY'
+global.ownerNumber = ["6285377536527@s.whatsapp.net"]
+global.email = '-'
+global.yt = '-'
+global.webkir = 'https://wa.me/6285377536527'
+global.githubkir = '-'
+global.region = 'Indonesia'
+global.prefa = ['','!','.','#','-','•']
+global.mess = {
+    success: 'Success 💯',
+    admin: 'Fitur Khusus Admin Group!!!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!!!',
+    owner: 'Fitur Khusus Owner Bot!!!',
+    group: 'Fitur Digunakan Hanya Untuk Group!!!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!!!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot!!!',
+    error: 'Error Kak Silahkan Chat Owner',
+    wait: '❗Waittt...',
+    noPetualang: 'Itssss Kamu Belum Jadi Member Petualang, Silahkan Ketik .joinrpg <namamu>',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+    premium: 'Maaf Sebelumya Kamu Belum Premium, Silahkan Pencet Di Bawah Untuk Beli Premium',
+    wrongFormat: 'Format salah, coba liat lagi di menu',
+}
+
+
+global.limitawal = {
+    premium: "Infinity",
+    free: 15
+}
+
+
+global.thumb = fs.readFileSync('./Image/kirbotz.jpg')
+global.thumbdm = fs.readFileSync('./Image/diamond.jpg')
+global.thumbrpg = fs.readFileSync('./Image/kirbotzrpg.jpg')
+global.imgjoinrpg = fs.readFileSync('./Image/joinrpg.jpg')
+global.imgmining = fs.readFileSync('./Image/mining.jpg')
+global.sticknya = fs.readFileSync('./Kir/JANGANDIUBAH/fuck.webp')
+
+global.imgdevil = fs.readFileSync('./Image/img/devil.jpg')
+global.imgslime = fs.readFileSync('./Image/img/slime.jpg')
+global.imgdemon = fs.readFileSync('./Image/img/demon.jpg')
+global.imggoblin = fs.readFileSync('./Image/img/goblin.jpg')
+global.imgdemonking = fs.readFileSync('./Image/img/demonking.jpg')
+global.imgbehemoth = fs.readFileSync('./Image/img/behemoth.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
